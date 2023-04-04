747172675d229555fb915fa9ebdd3d6e150e0792
Block-master <cristiano.piscioneri@gmail.com>
2023-04-04 18:31:54 +0200

    remove unused

--- test/01-deploy.ts
+++ test/01-deploy.ts
@@ -1,20 +1,11 @@
 import { deployments, ethers, network } from "hardhat"
 import { Gasless } from "../typechain-types"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-// import { use, providers }  from "@nomiclabs/hardhat"
-import { EthereumProvider } from "hardhat/types"
-import { expect } from "chai"
-import * as abi from '../artifacts/contracts/Gasless.sol/Gasless.json'
-import { GsnTestEnvironment } from "@opengsn/dev"
+
 describe("GSN", () => {
   let gasless: Gasless
-  let abi: Gasless
   let deployer: SignerWithAddress
-  let supervisor: SignerWithAddress
-  let legal: SignerWithAddress
-  let teacher: SignerWithAddress
   let foreign: SignerWithAddress
-  let provider: EthereumProvider
   
   beforeEach(async () => {
     await deployments.fixture(["all"])
